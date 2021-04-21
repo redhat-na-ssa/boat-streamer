6c44c89b01f8e0edca02c2046b53d595d291f44c
Ryan Kraus <rmkraus@gmail.com>
2021-04-21 19:33:38 -0400

    Changed to HTTP streams instead of RTSP.

--- stream.sh
+++ stream.sh
@@ -5,11 +5,11 @@ if ! [ -f "$1" ]; then
     exit 1
 fi
 
-echo "Starting RTSP Server"
+echo "Starting HTTP Server"
 sleep 2
 
 vlc \
-    --sout '#duplicate{dst=gather:rtp{sdp=rtsp://:8080/video.sdp},dst=display}' \
+    --sout '#duplicate{dst=gather:http{mux=ffmpeg{mux=flv},dst=:8080/},dst=display}' \
     --no-sout-audio \
     --sout-keep \
     --marq-marquee "Live Camera" \
@@ -24,7 +24,7 @@ sleep 5
 printf "\n\n\n\n"
 echo "VLC is running with PID: ${vlc_pid}"
 for ip in $(ip a | grep inet | awk '{ print $2 }' | awk -F/ '{ print $1 }'); do
-    echo "Server is accessible at rtsp://$ip:8080/video.sdp"
+    echo "Server is accessible at http://$ip:8080/"
 done
 
 while kill -0 $vlc_pid &> /dev/null; do
